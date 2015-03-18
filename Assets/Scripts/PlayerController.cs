bbafd0a07f7784f939468b20fcbeab672cfd8411
Revoori Sai Revanth <revanth001reva@gmail.com>
2015-03-18 18:39:32 +0530

    Stage 8 Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin,xMax,zMin,zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void FixedUpdate() {
+		float horizantal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (horizantal, 0.0f, vertical);
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x,boundary.xMin,boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z,boundary.zMin,boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
